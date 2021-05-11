1b4e20bd9377a16588a29e70db179edbf3184e2f
ShijiaYan <45582332+ShijiaYan@users.noreply.github.com>
2021-05-11 09:42:13 -0400

    first commit

---
+++ run_DEVDAN.m
@@ -0,0 +1,32 @@
+% LicenseCC BY-NC-SA 4.0
+% 
+% Copyright (c) 2018 Andri Ashfahani Mahardhika Pratama
+
+clc
+clear
+close all
+
+% All the datasets used in our experiments can be downloaded in this link: https://bit.ly/2mhtRsE
+
+% load rotatedmnist; 	I = 784;
+% load permutedmnist; 	I = 784;
+load mnist.mat; 		I = 784;
+% load forestcovtype; 	I = 54;
+% load sea; 		    I = 3;
+% load hyperplane;  	I = 4;
+% load occupancy; 	    I = 5;
+% load kddcup; 		    I = 41;
+% load Hepemass; 	    I = 27;
+
+%% This is ONLY for the numerical example where number of labeled data is limited, section 4.9
+selectiveSample.mode  = 0;       % 0: selective sample off, 1: selective sample on
+selectiveSample.delta = 0.55;    % confidence level; 
+portionOfLabeledData  = 1;       % 0-1
+
+%% DEVDAN
+chunkSize = 1000;       % number of data in a batch
+mode      = 0;          % 0: all components are on, 1: generative off, 2: growing hidden unit off, 3: pruning hidden unit off
+[parameter,performance] = DEVDAN(data,I,portionOfLabeledData,mode,...
+    selectiveSample,chunkSize);
+clear data
+disp(performance)
